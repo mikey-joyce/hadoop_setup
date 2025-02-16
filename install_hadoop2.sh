8a25d0586b97f8d97f9068bcc9eb00f21378e826
mikey-joyce <joyce.mp2019@gmail.com>
2025-02-16 15:27:00 -0600

    Split Hadoop Install

--- install_hadoop2.sh
+++ install_hadoop2.sh
@@ -2,28 +2,6 @@
 
 set -e  # Exit if any command fails
 
-echo "Updating package lists..."
-sudo apt update
-
-echo "Installing OpenJDK 8..."
-sudo apt install -y openjdk-8-jdk
-
-echo "Installing SSH..."
-sudo apt install -y ssh
-
-echo "Installing net-tools..."
-sudo apt install -y net-tools
-
-echo "Generating SSH key..."
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
-
-echo "Adding public key to authorized_keys..."
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-chmod 640 ~/.ssh/authorized_keys  # Set the correct permissions for security
-
-echo "Testing SSH connection to localhost..."
-ssh -o StrictHostKeyChecking=no localhost  # Skip first-time host key prompt
-
 echo "Downloading Hadoop 2.8.1 tarball..."
 wget https://archive.apache.org/dist/hadoop/core/hadoop-2.8.1/hadoop-2.8.1.tar.gz
 
