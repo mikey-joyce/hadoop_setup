8b4c1a18c4074ebadef25a644337836de520db2b
Mikey Joyce <joyce.mp2019@gmail.com>
2025-02-18 12:49:55 -0600

    Spark download

---
+++ spark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Download Apache Spark 2.2.0
+wget https://archive.apache.org/dist/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz
+
+# Extract the downloaded archive
+tar xvfz spark-2.2.0-bin-hadoop2.7.tgz
+
+# Rename extracted directory
+mv spark-2.2.0-bin-hadoop2.7 spark
+
+# Add SPARK_HOME to bashrc and source it
+echo 'export SPARK_HOME=/home/ubuntu/spark' >> ~/.bashrc
+source ~/.bashrc
+
+echo 'Spark install complete!'
