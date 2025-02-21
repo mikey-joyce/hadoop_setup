5861d33a73dd437865e58872626f6b031a1716ec
mikey-joyce <joyce.mp2019@gmail.com>
2025-02-20 18:44:50 -0600

    Update spark_wordcount.py

--- code/spark_wordcount.py
+++ code/spark_wordcount.py
@@ -4,9 +4,9 @@ def main():
     conf = SparkConf().setAppName("WordCount").setMaster("local[*]")
     sc = SparkContext(conf=conf)
 
-    input_files = "/phase1/hashtags_urls/*.txt"
+    input_files = "/phase1/hashtags_urls/*"
     text_files = sc.textFile(input_files)
-    text_files = text_files.filter(lambda x: "_SUCCESS.txt" not in x)
+    text_files = text_files.filter(lambda x: "_SUCCESS" not in x)
 
     words = text_files.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1))
     counts = words.reduceByKey(lambda x, y: x + y)
