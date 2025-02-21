6434a190b7140c520da7eaaf385c63c6806be4eb
mikey-joyce <joyce.mp2019@gmail.com>
2025-02-20 18:41:44 -0600

    Spark word count

---
+++ code/spark_wordcount.py
@@ -0,0 +1,22 @@
+from pyspark import SparkContext, SparkConf
+
+def main():
+    conf = SparkConf().setAppName("WordCount").setMaster("local[*]")
+    sc = SparkContext(conf=conf)
+
+    input_files = "/phase1/hashtags_urls/*.txt"
+    text_files = sc.textFile(input_files)
+
+    words = text_files.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1))
+    counts = words.reduceByKey(lambda x, y: x + y)
+
+    results = counts.collect()
+    for word, count in results:
+        print(f"{word}: {count}")
+
+    results.saveAsTextFile("/phase1/results/spark_wordcount")
+
+    sc.stop()
+
+if __name__ == "__main__":
+    main()
