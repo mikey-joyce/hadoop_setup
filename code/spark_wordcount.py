5b452be2e4acf1aff534a0231879b8b3d9be16df
mikey-joyce <joyce.mp2019@gmail.com>
2025-02-20 18:42:22 -0600

    Ooops

--- code/spark_wordcount.py
+++ code/spark_wordcount.py
@@ -6,6 +6,7 @@ def main():
 
     input_files = "/phase1/hashtags_urls/*.txt"
     text_files = sc.textFile(input_files)
+    text_files = text_files.filter(lambda x: "_SUCCESS.txt" not in x)
 
     words = text_files.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1))
     counts = words.reduceByKey(lambda x, y: x + y)
