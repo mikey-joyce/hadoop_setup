4f5c3998388b3513b04e4a7a0dd1285918aacc18
Mikey Joyce <59045943+mikey-joyce@users.noreply.github.com>
2025-03-15 13:32:46 -0500

    Update pyspark_ray.sh

--- install/pyspark_ray.sh
+++ install/pyspark_ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install python3-pip
-pip install --upgrade setuptools
+python3 -m pip install --upgrade setuptools pip
 python3 -m pip install --upgrade pip
 
 # Install PySpark
