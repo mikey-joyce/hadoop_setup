a6a2c354779b63717c27ca1f113c13ad26236449
Mikey Joyce <59045943+mikey-joyce@users.noreply.github.com>
2025-03-15 13:18:52 -0500

    Update parse_data.py

--- code/parse_data.py
+++ code/parse_data.py
@@ -1,25 +1,54 @@
 from pyspark import SparkContext, SparkConf
 import pandas as pd
-from io import StringIO
+from io import StringIO, BytesIO
 
 def main():
-    conf = SparkConf().setAppName("ParseData").setMaster("local[*]")
+    conf = SparkConf().setAppName("ParseData").set("spark.driver.memory", "6g").set("spark.executor.memory", "6g")
     sc = SparkContext(conf=conf)
 
-    sc.setLogLevel("INFO")
+    sc.setLogLevel("OFF")
 
-    data_path = '/phase2/data/*'
-
+    data_path = 'hdfs://localhost:9000/phase2/data/'
     files = sc.wholeTextFiles(data_path)
-    parsed = files.mapValues(process_with_pandas)
+
+    print()
+    print("FILES: ", files)
+    print()
+
+    inp = []
+    for filename, content in files.take(11):
+        print(f"Detected filename: {filename}")
+        print("Content: ", content)
+
+    parsed = files.mapValues(lambda file: (file[0], detect_and_parse(file[1], file[0])))
+
+    print()
+    print("PARSED: ", parsed)
+    print()
+
     dfs = parsed.collectAsMap()
 
     print(len(dfs))
 
 
-def process_with_pandas(file_content):
-    """Convert CSV content into a Pandas DataFrame."""
-    return pd.read_csv(StringIO(file_content))
+def detect_and_parse(content, filename):
+    filename = filename.lower().strip()
+    print(f"Processing {filename}...")
+
+    try:
+        if filename.endswith(".csv"):
+            return pd.read_csv(StringIO(content), on_bad_lines="skip")
+        elif filename.endswith(".json"):
+            return pd.read_json(StringIO(content))
+        elif filename.endswith(".xls") or filename.endswith(".xlsx"):
+            return pd.read_excel(BytesIO(content.encode("utf-8")))
+        else:
+            print(f"Unsupported file format: {filename}")
+            return None
+    except Exception as e:
+        print(f"Error processing {filename}: {e}")
+        return None
+
 
 if __name__ == "__main__":
     main()
