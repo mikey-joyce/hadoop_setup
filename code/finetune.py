45b982c409402c4a7575b70b13807ff2cc5a0e3d
Mikey Joyce <joyce.mp2019@gmail.com>
2025-04-18 15:06:20 -0500

    Update
    collect was too slow

--- code/finetune.py
+++ code/finetune.py
@@ -6,10 +6,9 @@ import time
 def main():
     spark = SparkSession.builder.appName("ReadTrain").getOrCreate()
     rdd = spark.sparkContext.textFile("hdfs:///phase2/data/train")
-    train = rdd.collect()
-    train = rd.from_items(train)
     print("HELLO!")
     time.sleep(10)
+    train = rd.from_items(rdd.toLocalIterator())
     train.show(5)
     time.sleep(60)
 
