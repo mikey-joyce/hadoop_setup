417e64a2563e0bfe5be856c5c6fc0929647e4afd
Mikey Joyce <joyce.mp2019@gmail.com>
2025-04-19 13:26:41 -0500

    Ray is weird

--- code/finetune.py
+++ code/finetune.py
@@ -21,7 +21,7 @@ logger.setLevel(logging.INFO)
 
 def tokenize_function(examples, tokenizer):
         logger.info(f"Examples preview:\n{examples}")
-        examples.show(5)
+        print(examples)
         return tokenizer(examples["content"], padding="max_length", truncation=True)
 
 
