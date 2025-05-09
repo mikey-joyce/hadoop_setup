b76681fe14dd0492a964793a8cac8d72d83082db
Zayan <zayan.qazi.mail@gmail.com>
2025-05-09 20:46:16 +0000

    pushed retrained model

--- code/push_model.py
+++ code/push_model.py
@@ -14,7 +14,7 @@ def push_model_and_tokenizer(checkpoint_path, repo_id, commit_message=None):
     print("Model pushed to Hugging Face Hub successfully.")
     
     # push tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(f"{checkpoint_path}/tokenizer.json") 
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint_path) 
     tokenizer.push_to_hub(repo_id)
 
     print("Tokenizer pushed to huggingface hub successfully")
@@ -29,7 +29,7 @@ def push_checkpoint_directory(checkpoint_dir, repo_id, repo_type):
 
 if __name__ == "__main__":
     # push model and tokenizer
-    path = "~/ray_results/zayanhugsAI/twitter_roberta_finetuned/TorchTrainer_f0b32_00000_0_2025-05-06_02-00-52/checkpoint_000002/checkpoint"
+    path = "/home/ubuntu/ray_results/zayanhugsAI/twitter_roberta_finetuned/TorchTrainer_f0b32_00000_0_2025-05-06_02-00-52/checkpoint_000002/checkpoint"
 
     # push checkpoint dir
     # checkpoint_dir = "~/ray_results/twitter-roberta-finetune/TorchTrainer_4639e_00000_0_2025-05-02_15-36-54/checkpoint_000004/checkpoint/"
